0bdfa20627cedcfe37353ae4e556b1fa23741a99
Marcus Vinicius Nascimento Ferreira <dev.mvinicius@gmail.com>
2026-01-04 22:24:18 -0300

    Refactor HomeMediumPage layout and update button components

--- lib/pages/HomeMediumPage.dart
+++ lib/pages/HomeMediumPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:medium_home/widgets/custom_buttom.widget.dart';
+import 'package:medium_home/widgets/social_custom.widget.dart';
 
 class HomeMediumPage extends StatelessWidget {
   const HomeMediumPage({super.key});
@@ -28,20 +29,52 @@ class HomeMediumPage extends StatelessWidget {
                 ),
               ),
 
-              SizedBox(height: 40),
+              SizedBox(height: 48),
 
-              CustomButton(
+              SocialButton(
                 text: 'Sign up with Google',
                 pathImage: 'images/assets/google.png',
                 key: ValueKey("signupGoogle"),
               ),
 
-              SizedBox(height: 10),
+              SizedBox(height: 16),
 
-              CustomButton(
+              SocialButton(
                 text: 'Sign up with email',
                 pathImage: 'images/assets/email.png',
                 key: ValueKey("signupEmail"),
+              ),
+
+              SizedBox(height: 40),
+
+              Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      thickness: 1,
+                      color: Colors.black,
+                      key: ValueKey("dividerLeft"),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Text("Or, sign up with"),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      thickness: 1,
+                      color: Colors.black,
+                      key: ValueKey("dividerRight"),
+                    ),
+                  ),
+
+                  SizedBox(height: 40),
+                ],
+              ),
+
+              CircleSocialButton(
+                pathImage: 'images/assets/facebook.png',
+                key: ValueKey("buttonFacebook"),
               ),
             ],
           ),
